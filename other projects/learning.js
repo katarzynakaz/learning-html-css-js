1c43958349f4b0155cdb93532dc5c930483e97f7
k45k4k4 <l453r3@gmail.com>
2023-11-20 15:27:28 +0000

    Update learning.js

--- other projects/learning.js
+++ other projects/learning.js
@@ -1,6 +1,6 @@
 function whoIsPaying(names) {
     var numberOfPeople = names.length;
-    var randomPersonPosition = Math.floor (Math.random() * numberOfPeople);
-    var randomName = names [randomPersonPosition];
+    var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+    var randomName = names[randomPersonPosition];
     return randomName = " is going...."
 }
