e0d9092ded32ded8863a6f9d3e9c9d27bb16186b
k45k4k4 <l453r3@gmail.com>
2023-11-23 11:00:32 +0000

    dom

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = getRandom;
+
+function getRandom(1, 6) {
+    return Math.floor(Math.random() * (max - min +1) + min));
+}
+
+// Use the random number you created in the last step to pick out a 
+// random dice image between dice1.png to dice 6.png then place this image inside the left <img> element.
+
+// create array of images
+
+
+//select random image from array
+document.querySelector("img1").setAttribute("a href = ")
