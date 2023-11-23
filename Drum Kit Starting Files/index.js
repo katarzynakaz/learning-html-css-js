4b610d1fad538f5fd4bbafb1ede9324559f21673
k45k4k4 <l453r3@gmail.com>
2023-11-23 18:24:29 +0000

    js course

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,14 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// create for (var i = 0, i<document.querySelectorAll(".drum").length) 
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+
+function handleClick() {
+    alert("I got clicked");
+}
+
+// while btn clieed run handleClick
