4cfc8b0d18be64279e0e4e5c90fa095221aa7af0
k45k4k4 <l453r3@gmail.com>
2023-11-24 16:19:38 +0000

    simon game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,24 @@
+// alert("hi");
+// 3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// 1. Inside game.js create a new function called nextSequence()
+nextSequence() {
+    // 2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+var randomNumber = Math.floor(Math.random * 3);
+
+}
+// 4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+// var randomChosenColor = {
+//     for (var i = 0; i < 4; i++)
+    var randomColor = buttonColours[Math.floor(Math.random() * items.length)];
+
+
+
+// 5. At the top of the game.js file, create a new empty array called gamePattern.
+
+
+
+// 6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+
+
