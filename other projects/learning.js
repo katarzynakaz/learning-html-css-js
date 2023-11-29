35a3f7d660dd100f6f1fd29273f1fc1dff46e4d3
k45k4k4 <l453r3@gmail.com>
2023-11-29 13:32:52 +0000

    update notes

--- other projects/learning.js
+++ other projects/learning.js
@@ -142,3 +142,101 @@ function switchOfStuff(val) {
   }
   
   switchOfStuff(1);
+
+  analogy
+//   function switchOfStuff(val) {
+//   let answer = "";
+//   // Think of 'val' as the label on a key
+//   switch (val) {
+//     case "a": 
+//       // If the key is labeled 'a', it opens a box with an apple
+//       answer = "apple";
+//       break;
+//     case "b":
+//       // If the key is labeled 'b', it opens a box with a bird
+//       answer = "bird";
+//       break;
+//     case "c":
+//       // If the key is labeled 'c', it opens a box with a cat
+//       answer = "cat";
+//       break;
+//     default: 
+//       // Any other label, the key opens a box with 'stuff'
+//       answer = "stuff";
+//       break;
+//   }
+//   // Return the content of the opened box
+//   return answer;
+// }
+
+// switchOfStuff(1); // This will return 'stuff' as the key labeled '1' opens the mystery box
+
+sweitch instead of what if 
+in switch number ()
+string " "
+empty ""
+
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+      case "bob":
+      answer = "Marley";
+      break;
+      case (42):
+      answer = "The Answer";
+      break;
+      case (1):
+      answer = "There is no #1";
+      break;
+      case (99):
+      answer = "Missed me by this much!";
+      break;
+      case (7):
+      answer = "Ate Nine";
+      break;
+      case "John":
+      answer = "";
+      break;
+      case (156):
+      answer = "";
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+
+
+
+
+  Booleanfunction isDarkOutside(a, b) {
+    // 'a' is the current time, 'b' is sunset time
+    // The function checks if it's dark outside (if the current time is later than sunset time)
+    // It's like an automatic light switch:
+    // If it's dark (current time is later than sunset time), the light turns on (returns true)
+    // If it's not dark (current time is earlier than sunset time), the light stays off (returns false)
+    return a > b; // Automatically returns true if it's dark, false if it's not
+  }
+  
+  // Example usage:
+  isDarkOutside(21, 18); // Is it dark outside at 9 PM if sunset is at 6 PM? Returns true.
+
+
+  better analogyunction hasFinishedFirst(runnerA, runnerB) {
+    if (runnerA < runnerB) {
+      return true;  // Runner A finished first
+    } else {
+      return false; // Runner B finished first or they finished at the same time
+    }
+  }
+
+  can be shown as
+
+  
+  function hasFinishedFirst(runnerA, runnerB) {
+    // Directly check if Runner A's time is less than Runner B's time
+    // It's like directly knowing who crossed the finish line first without needing a judge to decide
+    return runnerA < runnerB;
+  }
