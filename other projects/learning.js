bab2f7989761aa77833879553424b9ed128724fe
k45k4k4 <l453r3@gmail.com>
2023-11-29 13:09:06 +0000

    notes added

--- other projects/learning.js
+++ other projects/learning.js
@@ -55,3 +55,90 @@ if (numberOfCats > 5) {
   }
 
   else if is more specific 
+  
+
+
+  function feedLaserTreats(plannedTreats, actualTreats) {
+    if (actualTreats === 1) {
+      return "Hole-in-one!";
+    } else if (actualTreats <= plannedTreats - 2) {
+      return "Eagle";
+    } else if (actualTreats === plannedTreats - 1) {
+      return "Birdie";
+    } else if (actualTreats === plannedTreats) {
+      return "Par";
+    } else if (actualTreats === plannedTreats + 1) {
+      return "Bogey";
+    } else if (actualTreats === plannedTreats + 2) {
+      return "Double Bogey";
+    } else {
+      return "Go Home!";
+    }
+  }
+
+
+//   switch
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+      case 1 :
+    answer = "alpha";
+    break;
+   case 2:
+    answer = "beta";
+    break;
+    case 3:
+    answer = "gamma";
+    break;
+    case 4:
+    answer = "delta";
+    break;
+    }
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
+
+//   function chooseBeverage(mood) {
+//   let beverage;
+//   switch (mood) {
+//     case "happy":
+//       beverage = "Smoothie";
+//       break;
+//     case "tired":
+//       beverage = "Coffee";
+//       break;
+//     case "hot":
+//       beverage = "Iced Tea";
+//       break;
+//     case "cold":
+//       beverage = "Hot Chocolate";
+//       break;
+//   }
+//   return beverage;
+
+with default 
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+      case "a": 
+      answer = "apple";
+      break;
+      case "b":
+      answer = "bird";
+      break;
+      case "c":
+      answer = "cat";
+      break;
+      default: 
+      answer = "stuff";
+      break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  
+  switchOfStuff(1);
