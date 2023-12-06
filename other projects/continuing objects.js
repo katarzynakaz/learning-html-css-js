88056fd2d171963e107535a6fb851f2b8a841bbc
k45k4k4 <l453r3@gmail.com>
2023-12-06 00:04:44 +0000

    learning objects

---
+++ other projects/continuing objects.js
@@ -0,0 +1,47 @@
+Exercise: Pet Store Inventory Management
+Scenario: You're managing a pet store's inventory. The store keeps various types of pets, each identified by a unique ID. For each pet, the store records its type (like "dog", "cat", "fish"), and the quantity of that pet type currently in stock.
+
+The petStore inventory object could look like this:
+
+javascript
+Copy code
+let petStore = {
+    1: { type: "dog", quantity: 5 },
+    2: { type: "cat", quantity: 3 },
+    3: { type: "fish", quantity: 10 }
+};
+Task: Write a function updateInventory that takes three parameters: petStore (the inventory object), petID (the ID of the pet type), and additionalQuantity (the number of new pets of that type received by the store). The function should update the quantity of the specified pet type by adding the additionalQuantity. If the pet type with the given petID does not exist in the petStore, the function should add a new entry with petID, an assumed type of "Unknown", and the additionalQuantity as its stock.
+
+Return the updated petStore inventory after the operation.
+
+
+
+
+
+
+Exercise: Managing a Plant Nursery
+Scenario: You manage a plant nursery, and you need to keep track of various plants and their watering schedules. Each plant is identified by a unique ID, and for each plant, you store the last date it was watered.
+
+The nursery object looks like this:
+
+javascript
+Copy code
+let nursery = {
+    1: { plantName: "Fern", lastWatered: "2023-10-01" },
+    2: { plantName: "Cactus", lastWatered: "2023-10-05" }
+};
+Task: Write a function updateWateringSchedule that takes three parameters: nursery (the object), plantID (the ID of the plant), and wateringDate (the date when the plant was last watered). The function should update the lastWatered date for the specified plant. If the plant with the given plantID does not exist in the nursery, the function should add a new plant entry with plantID, an assumed plantName of "Unknown", and the wateringDate.
+
+Your function should then return the updated nursery object.
+function updateWateringSchedule(nursery, plantID, wateringDate) {
+    // Check if the plant with the given plantID exists in the nursery
+    if (!nursery[plantID]) {
+        // If the plant doesn't exist, create a new entry with the plantID
+        nursery[plantID] = { plantName: "Unknown", lastWatered: wateringDate };
+    } else {
+        // If the plant exists, update the lastWatered date
+        nursery[plantID].lastWatered = wateringDate;
+    }
+
+    return nursery;
+}
