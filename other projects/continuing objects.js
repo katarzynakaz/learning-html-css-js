20eaf62e5966f0484207123db5209a0b08adb540
k45k4k4 <l453r3@gmail.com>
2023-12-06 00:32:41 +0000

    Update continuing objects.js

--- other projects/continuing objects.js
+++ other projects/continuing objects.js
@@ -1,18 +1,113 @@
 Exercise: Pet Store Inventory Management
-Scenario: You're managing a pet store's inventory. The store keeps various types of pets, each identified by a unique ID. For each pet, the store records its type (like "dog", "cat", "fish"), and the quantity of that pet type currently in stock.
+Scenario: You're managing a pet store's inventory. The store keeps 
+various types of pets, each identified by a unique ID. For each pet, the 
+store records its type (like "dog", "cat", "fish"), and the quantity of that pet 
+type currently in stock.
 
 The petStore inventory object could look like this:
 
-javascript
-Copy code
+
 let petStore = {
     1: { type: "dog", quantity: 5 },
     2: { type: "cat", quantity: 3 },
     3: { type: "fish", quantity: 10 }
 };
-Task: Write a function updateInventory that takes three parameters: petStore (the inventory object), petID (the ID of the pet type), and additionalQuantity (the number of new pets of that type received by the store). The function should update the quantity of the specified pet type by adding the additionalQuantity. If the pet type with the given petID does not exist in the petStore, the function should add a new entry with petID, an assumed type of "Unknown", and the additionalQuantity as its stock.
+Task: Write a function updateInventory that takes three parameters: petStore (the inventory object), 
+petID (the ID of the pet type), and additionalQuantity (the number of new pets of that type received by 
+    the store). The function should update the quantity of the specified pet type by adding the 
+    additionalQuantity. If the pet type with the given petID does not exist in the petStore, the 
+    function should add a new entry with petID, an assumed type of "Unknown", and the additionalQuantity
+     as its stock.
 
 Return the updated petStore inventory after the operation.
+
+
+// function updateInventory (petStore, petID, additionalQuantitiy) {
+//     if (!petStore[petID]) {
+//         petStore[petID] = { petID: "unknown", stock: additionalQuantity}
+//     } else {
+//     let  quantity =+ additionalQuantitiy;
+//     }
+// return petStore;
+// }
+
+function updateInventory(petStore, petID, additionalQuantity) {
+    if (!petStore[petID]) {
+        // Add a new pet type entry if it does not exist
+        petStore[petID] = { type: "Unknown", quantity: additionalQuantity };
+    } else {
+        // Update the quantity for an existing pet type
+        petStore[petID].quantity += additionalQuantity;
+    }
+
+    return petStore;
+}
+
+
+
+
+
+
+Exercise: Classroom Attendance Tracker
+Scenario: You are managing a classroom's attendance. Each student in the classroom is identified by a unique
+ ID. For each student, the classroom records their name and a list of dates when they were present.
+
+The classroom object might look like this:
+
+let classroom = {
+    1: { name: "Alice", attendance: ["2023-09-01", "2023-09-02"] },
+    2: { name: "Bob", attendance: ["2023-09-01"] }
+};
+
+
+Task: Write a function markAttendance that takes three parameters: classroom (the attendance object), 
+studentID (the ID of the student), and date (the date of attendance to be marked). The function should add 
+the date to the student's attendance array. If the student with the given studentID does not exist in the 
+classroom, the function should add a new student entry with studentID, an assumed name of "Unknown", and the
+ date in their attendance.
+
+Return the updated classroom attendance after the operation.
+
+
+// function markAttendance (classroom, studentID, date) {
+//     if (!classroom[studentID]) {
+//        classroom[studentID] = {name: "Unknown", attendance: [date]}
+//     }
+//     else {
+//         classroom[studentID][attendance].push(date);
+//     }
+// return classroom;
+// }
+
+
+function markAttendance(classroom, studentID, date) {
+    if (!classroom[studentID]) {
+        // Add a new student entry if it does not exist
+        classroom[studentID] = { name: "Unknown", attendance: [date] };
+    } else {
+        // Add the date to the attendance array for an existing student
+        classroom[studentID].attendance.push(date);
+    }
+
+    return classroom;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -24,8 +119,6 @@ Scenario: You manage a plant nursery, and you need to keep track of various plan
 
 The nursery object looks like this:
 
-javascript
-Copy code
 let nursery = {
     1: { plantName: "Fern", lastWatered: "2023-10-01" },
     2: { plantName: "Cactus", lastWatered: "2023-10-05" }
