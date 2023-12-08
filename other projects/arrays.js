c689e618ad417c87c1a620bb423cd3f27307484d
k45k4k4 <l453r3@gmail.com>
2023-12-08 14:25:38 +0000

    Update arrays.js

--- other projects/arrays.js
+++ other projects/arrays.js
@@ -30,3 +30,27 @@ function countBooks(bookstoreNetwork) {
     
     return totalBooks;
 }
+
+
+
+function sum(arr, n) {
+    // Only change code below this line
+  // Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+  if (n <= 0) {
+    return 0; 
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+    
+  
+    // Only change code above this line
+  }
+
+
+  function totalZits(arr, n) {
+    if (n <= 0) {
+      return 0; // Base case: no patients left to treat.
+    } else {
+      return totalZits(arr, n - 1) + arr[n - 1]; // Treat the first patient and add it to the total for the rest.
+    }
+  }
