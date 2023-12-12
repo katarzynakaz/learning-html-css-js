e1257c03188a6d5a939d2737b0edaa5b2bf62813
k45k4k4 <l453r3@gmail.com>
2023-12-12 11:49:35 +0000

    Update objects.js

--- other projects/objects.js
+++ other projects/objects.js
@@ -590,3 +590,79 @@ let newBook = { title: "The Catcher in the Rye", author: "J.D. Salinger", isAvai
 library = manageLibrary(library, 4, 'add', newBook);
 // After adding a book, library should include the new book.
 
+
+Object constructor 
+
+Extend Constructors to Receive Arguments
+The Bird and Dog constructors from the last challenge worked well. However, notice that all Birds 
+that are created with the Bird constructor are automatically named Albert, are blue in color, and 
+have two legs. What if you want birds with different values for name and color? 
+It's possible to change the properties of each bird manually but that would be a lot of work:
+
+let swan = new Bird();
+swan.name = "Carlos";
+swan.color = "white";
+Suppose you were writing a program to keep track of hundreds or even thousands of different birds 
+in an aviary. It would take a lot of time to create all the birds, then change the properties to
+ different values for every one. To more easily create different Bird objects, you can design your
+  Bird constructor to accept parameters:
+
+function Bird(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+Then pass in the values as arguments to define each unique bird into the Bird constructor: 
+let cardinal = new Bird("Bruce", "red"); This gives a new instance of Bird with name and color 
+properties set to Bruce and red, respectively. The numLegs property is still set to 2. The cardinal 
+has these properties:
+
+cardinal.name
+cardinal.color
+cardinal.numLegs
+The constructor is more flexible. It's now possible to define the properties for each Bird at the time 
+it is created, which is one way that JavaScript constructors are so useful. They group objects together
+ based on shared characteristics and behavior and define a blueprint that automates their creation.
+
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+let terrier = new Dog("Pumpkin", "orange")
+
+
+
+
+// instance of to check if instance is a baby of constructor
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(3);
+myHouse instanceof House;
+
+
+
+// Own properties
+- defined directly on the instance object 
+// as instance created from blueprint constructor
+
+fucntion Cat(name) {
+  this.name = name;
+  this.numLegs = 4;
+}
+
+let ragdoll = new Cat("Laser");
+let tabby = new Cat("Tosia");
+// ?every instance of cat has its own copy of properties
+
+// ?to add all props to array
+
+let ownProps = [];
+for (let property in cat) {
+  if(cat.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
