853c89965ff40eb422160b2c725d61bb27da0e0c
k45k4k4 <l453r3@gmail.com>
2023-12-12 18:01:57 +0000

    Update objects.js

--- other projects/objects.js
+++ other projects/objects.js
@@ -817,3 +817,22 @@ function Cat() {}
 Cat.prototype = Object.create(Animal.prototype);
 let ragdoll = new Cat();
 
+// Reset an Inherited Constructor Property
+// if object inherits prototype it also inherits supertypoe;s constructor property 
+
+
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+let beagle = new Dog();
