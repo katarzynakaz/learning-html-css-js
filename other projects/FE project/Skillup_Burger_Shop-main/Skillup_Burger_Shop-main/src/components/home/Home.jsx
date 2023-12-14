38128ed94103f78fb50ca82e4ec02edb963b9827
k45k4k4 <l453r3@gmail.com>
2023-12-14 00:08:05 +0000

    Update Home.jsx

--- other projects/FE project/Skillup_Burger_Shop-main/Skillup_Burger_Shop-main/src/components/home/Home.jsx
+++ other projects/FE project/Skillup_Burger_Shop-main/Skillup_Burger_Shop-main/src/components/home/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 
-// Create a navigation bar with links to Home, Contact, About, shopping cart, and a menu with links to Login,
-// Orders and Logout
+
 
 
 const Home = () => {
