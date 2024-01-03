88c1b47c638065a61f3863782e3efb4da4494589
k45k4k4 <l453r3@gmail.com>
2024-01-03 11:37:49 +0000

    notes updated

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -14,6 +14,8 @@ var randomImageSource = "Images/" + randomDiceImage; //images/dice 1-6
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+// 1st inpit atribute name, 2nd parameter, what changed to 
+// image1.setAttribute("class", one);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -30,7 +32,6 @@ if (randomNumber1 > randomNumber2) {
 else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 wins!"
 }
-
-else if (randomNumber1 === randomNumber2) {
+else (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw! Try again!"
 }
--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,118 +1,118 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-// create for (var i = 0, i<document.querySelectorAll(".drum").length) 
+// // create for (var i = 0, i<document.querySelectorAll(".drum").length) 
+// // for (var i = 0; i<numberOfDrumButtons; i++) {
+
+// // document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+// // }
+
+
+// // function handleClick() {
+// //     alert("I got clicked");
+// // }
+
+// // while btn clieed run handleClick , can also be an annonymos function written as funciton() {}
+
+
+// // adding sound
+// // function that plays when button clicked using play audio inside
+
+// // longer way
+// // for (var i = 0; i<numberOfDrumButtons; i++) {
+// //     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+// // }
+
+// // function handleClick() {
+// // var audio = new Audio('tom-1.mp3');
+// // audio.play;
+// // }
+// // shotrter way
+
+
+
+// // detecting button press
 // for (var i = 0; i<numberOfDrumButtons; i++) {
-
-// document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-// }
-
-
-// function handleClick() {
-//     alert("I got clicked");
-// }
-
-// while btn clieed run handleClick , can also be an annonymos function written as funciton() {}
-
-
-// adding sound
-// function that plays when button clicked using play audio inside
-
-// longer way
-// for (var i = 0; i<numberOfDrumButtons; i++) {
-//     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-// }
-
-// function handleClick() {
-// var audio = new Audio('tom-1.mp3');
-// audio.play;
-// }
-// shotrter way
-
-
-
-// detecting button press
-for (var i = 0; i<numberOfDrumButtons; i++) {
     
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         
-        var buttonInnerHTML = this.innerHTML;
+//         var buttonInnerHTML = this.innerHTML;
         
-        makeSound(buttonInnerHTML);
-        buttonAnimation(buttonInnerHTML);
-});
-}
+//         makeSound(buttonInnerHTML);
+//         buttonAnimation(buttonInnerHTML);
+// });
+// }
         
-// detecting keyboard press
-document.addEventListener("keydown", function(event) {
+// // detecting keyboard press
+// document.addEventListener("keydown", function(event) {
 
-    makeSound(event.key);
-    buttonAnimation(event.key);
+//     makeSound(event.key);
+//     buttonAnimation(event.key);
     
-});
+// });
 
 
 
-function makeSound(key) {
-    switch (key) {
-        case "w":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
+// function makeSound(key) {
+//     switch (key) {
+//         case "w":
+//             var crash = new Audio("sounds/crash.mp3");
+//             crash.play();
 
-        break;
+//         break;
 
-        case "a":
-            var kickBass = new Audio("sounds/kick-bass.mp3");
-            kickBass.play();
+//         case "a":
+//             var kickBass = new Audio("sounds/kick-bass.mp3");
+//             kickBass.play();
 
-            break;
+//             break;
 
-        case "s":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
+//         case "s":
+//             var snare = new Audio("sounds/snare.mp3");
+//             snare.play();
 
-            break;
+//             break;
 
-        case "d":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
+//         case "d":
+//             var tom1 = new Audio("sounds/tom-1.mp3");
+//             tom1.play();
 
-            break;
+//             break;
 
-        case "j":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play(); 
+//         case "j":
+//             var tom2 = new Audio("sounds/tom-2.mp3");
+//             tom2.play(); 
 
-            break;
+//             break;
 
-        case "k":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
+//         case "k":
+//             var tom3 = new Audio("sounds/tom-3.mp3");
+//             tom3.play();
 
-            break;
+//             break;
 
-        case "l":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
+//         case "l":
+//             var tom4 = new Audio("sounds/tom-4.mp3");
+//             tom4.play();
 
-            break;
+//             break;
 
         
 
-        default: console.log(buttonInnerHTML);
-    };
-}
+//         default: console.log(buttonInnerHTML);
+//     };
+// }
 
-// animated button add remove class with a timeout
-function buttonAnimation(currentKey) {
-       var activeButton = document.querySelector("." + currentKey);
+// // animated button add remove class with a timeout
+// function buttonAnimation(currentKey) {
+//        var activeButton = document.querySelector("." + currentKey);
 
-       activeButton.classList.add("pressed");
+//        activeButton.classList.add("pressed");
 
-       setTimeOut(function() {
-         activeButtton.classList.remove("pressed");
-       }, 100); 
+//        setTimeOut(function() {
+//          activeButtton.classList.remove("pressed");
+//        }, 100); 
 
-    }
+//     }
 // above works in adding class, but does not remove class  
 // var audio = new Audio("sounds/tom-1.mp3");
 //         audio.play();
@@ -120,3 +120,27 @@ function buttonAnimation(currentKey) {
         // which button triggered event
         // this.style.color = "white";
         
+
+
+
+
+
+// /add event listener when clicked
+
+function handleClick() {
+    alert("I got clicked");
+}
+
+//button to trigger whern clicked 
+
+//add event listener in html and call function 
+
+document.querySelector("button").addEventListener("click", handleClick);
+// find first button in doc and add event listener to listen for clicks, when does runs handle click function
+// calling fucntion above without () as () run function immediately
+//instead of () calling the function imediately, we are passing the name of the function as an input to run when cliked
+
+
+
+
+
