3743c8241ce9834964b10e28375632b21170d1fa
k45k4k4 <l453r3@gmail.com>
2024-01-04 21:57:10 +0000

    notes updated

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,8 +1,15 @@
 // var generateName = require("sillyname");
 
 
-import generateName from "sillyname";
+// import generateName from "sillyname";
 
-var sillyName = generateName();
+// var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+
+console.log(`My superhero name is ${name}.`);
