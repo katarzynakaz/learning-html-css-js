70d154755cf5614de1c78861abfb9bd498df6728
k45k4k4 <l453r3@gmail.com>
2024-01-12 14:30:35 +0000

    scroll events wes

--- other projects/notes/wes.js
+++ other projects/notes/wes.js
@@ -45,10 +45,40 @@ function makeABaby(first, last) {
     return baby;
 }
 
-// resolution 
+
+
+// resolution notes
 // div canvas width and height twice as big as css (display on screen) 
 // for iphone or small cscreen high resolution px - double to get crisp look on page
 
+1600 1000 // from html actual canvas size gets logged, not css dieplay size
+const width = canvas.width;
+const height = canvas.height;
+
+console.log(width,height);
+
+
+// destructturing object inside function declarations
+// options instead of function(one, twom three, four, five +)
+function draw(options) {
+    console.log(options.key);
+}
+
+//destructure the object directly into variable 
+function draw({ key }) {
+    console.log(key);
+} 
+// to have a shorter variable name inside object and 
+
+
+const MOVE_AMOUNT = 10;
+// true constant in caps
+
+// replace all 10s with moveB
+ctx.lineWidth = 10;
+x = x - 10;
+// x -= 10 shorter
+y = y - 10;
 
 
 
